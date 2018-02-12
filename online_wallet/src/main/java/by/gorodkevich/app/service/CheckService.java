d2b7017f80ed6702c96a45cd70624092ae2044f0
dima-ПК\dima <dimagorodkevich@gmail.com>
2018-02-12 10:55:38 +0300

    delete

--- online_wallet/src/main/java/by/gorodkevich/app/service/CheckService.java
+++
@@ -1,6 +0,0 @@
-package by.gorodkevich.app.service;
-
-public interface CheckService {
-
-    void addCheck(Double money, String typeOperation);
-}
--- online_wallet/src/main/java/by/gorodkevich/app/service/IOMoneyInSystem.java
+++
@@ -1,9 +0,0 @@
-package by.gorodkevich.app.service;
-
-/**
- * Ввод/вывод денег в системе
- */
-public interface IOMoneyInSystem {
-
-    void inputMoney(Long toAccountNumber, Double amount);
-}
